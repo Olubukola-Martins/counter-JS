afd2fc017fcce6acdef25fe3f307d1950b79a6bf
Olubukola <19bukola97@gmail.com>
2023-03-28 15:58:29 +0100

    first method counter

---
+++ app.js
@@ -0,0 +1,20 @@
+// 'use strict';
+let counter = document.querySelector(".count");
+let count = 0;
+// const increase = document.querySelector(".Increase");
+// const decrease = document.querySelector(".Decrease");
+// const reset = document.querySelector(".Reset");
+
+// increase.addEventListener("click", function(){
+//  count ++;
+//  counter.textContent = count;
+// });
+// decrease.addEventListener("click", function(){
+//     count --;
+//     counter.textContent = count;
+//    });
+// reset.addEventListener("click", function(){
+//     counter.textContent = 0;
+//    });
+   
+
